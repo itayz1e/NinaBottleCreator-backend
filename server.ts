a752a127086f8e4f874fa014b1a137f5b9f3d7bb
itayz1e <itayz1e@gmail.com>
2024-01-24 21:54:38 +0200

    fix port

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@ import cors from "cors";
 require('dotenv').config()
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 app.use(express.json());
 app.use(cors());
 
