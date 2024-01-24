113cf4e33c84788932b86eee9977948d73ced0f1
itayz1e <itayz1e@gmail.com>
2024-01-24 22:29:35 +0200

    fix port

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@ import cors from "cors";
 require('dotenv').config()
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || "https://ninabottlecreator-backend.onrender.com"
 app.use(express.json());
 app.use(cors());
 
